ed02807420e7a1158558d094b0ac12b48fff630a
Jose Rubio <jadrubio@gmail.com>
2024-04-25 11:45:28 -0400

    urls and view for monthly challenges

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+
+monthly_challenges = {
+    "january": "Make a monthly challenge",
+    "february": "Hibernate",
+    "march": "Stand in the sun",
+    "april": "Learn Python",
+    "may": "Find a job",
+    "june": "Read a book",
+    "july": "Invent a recipe",
+    "august": "Exercise everyday",
+    "september": "Go to the beach",
+    "october": "Do one spooky thing everyday",
+    "november": "Be thankful",
+    "december": "Be kind",
+}
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge = monthly_challenges[month]
+        return HttpResponse(challenge)
+    except:
+        return HttpResponseNotFound("Hey! That's not a real month!")
